42d3846fab4e627ca4128e516f891b2df3edd5a8
Horaciob01 <horacio.b01@gmail.com>
2017-09-17 20:51:17 -0600

    arreglos

--- Monitores/src/mondatos/MonDatos.java
+++ Monitores/src/mondatos/MonDatos.java
@@ -1,17 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mondatos;
 
-import modelo.Conexion;
 import control.Control;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import ventanas.*;
-
-
 
 /**
  *
@@ -19,23 +9,12 @@ import ventanas.*;
  */
 public class MonDatos {
 
-    /**
-     * @param args the command line arguments
-     * @throws java.lang.InterruptedException
-     */
     public static void main(String[] args) throws InterruptedException, SQLException {
-        // TODO code application logic here
-          
-       // Tabla ta = new Tabla();
+        // Tabla ta = new Tabla();
         Control gestor = new Control();
         gestor.iniciar();
-          // crea la conexion con la base de datos 
-       // conecta a la base 
-       
-        
-        
-       // ta.toString();
-        
+        // crea la conexion con la base de datos 
+        // conecta a la base 
+        // ta.toString();
     }
-    
 }
